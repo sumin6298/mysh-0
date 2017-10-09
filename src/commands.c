294e633d2156a4a37cd5135c36616468ae1ffd91
Eosumin <sumin6206@naver.com>
2017-10-09 21:38:09 +0900

    //201421113 mysh 1. looking aroung the file system

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,51 @@
 #include <string.h>
-
+#include<unistd.h>
 #include "commands.h"
+#include<stdio.h>
 
 int do_cd(int argc, char** argv) {
+ 	
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  chdir(argv[1]);
+ 		
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
+
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+ char buf[100];
+  if(NULL!=getcwd(buf, 100)){
+	printf("dir: %s\n",buf);
+  }
+
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	if(argc == 2) {
+		if(strcmp(argv[0], "cd") == 0)
+			return 1;
+		else 
+			return 0;
+	}
+	else return 0;
+	
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	if(argc ==1){
+		if(strcmp(argv[0], "pwd") == 0 ) return 1;
+		else return 0;
+	}
+
+	else return 0;
 }
+	
+	
+
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,36 @@
 #include "utils.h"
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	char* command_clone;
+	char* word;
+	int WordNum = 0;
+	int i = 0;
+	command_clone = (char*)malloc(sizeof(char)*(strlen(command)+1));
+	strcpy(command_clone, command);
+
+	word = strtok(command_clone, " \t\n");
+
+	while(word != NULL){
+		WordNum++;
+		*argv = (char**)realloc(*argv, sizeof(char*)*(WordNum+1));
+		(*argv)[i] = (char*)malloc(sizeof(char)*(strlen(word)+1));
+		strcpy((*argv)[i], word);
+		word = strtok(NULL, " \t\n");
+		i++;
+	}
+	
+	
+	if(WordNum == 0){
+		WordNum++;
+		*argv = (char**)malloc(sizeof(char*)*(WordNum+1));
+		(*argv)[i] = (char*)malloc(sizeof(char)*2);
+		strcpy((*argv)[0], "");
+	}
+	*argc = WordNum;
+	
 }
